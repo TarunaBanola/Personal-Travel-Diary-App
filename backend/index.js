40cdfefa0b7e8516534b3ebb37083c475cd824a7
Taruna Banola <tarunabanola24@gmail.com>
2025-09-05 12:06:37 +0530

    create sign-in api route

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
 import authRoutes from "./routes/auth.route.js"
+import cookieParser from "cookie-parser"
 dotenv.config()
 mongoose.connect(process.env.MONGO_URI).then(
   ()=>{
@@ -11,6 +12,7 @@ mongoose.connect(process.env.MONGO_URI).then(
     console.log(err)
   })
 const app=express()
+app.use(cookieParser())
 app.use(express.json())
 app.listen(3000,()=>{
   console.log("Server is running on port 3000!")
